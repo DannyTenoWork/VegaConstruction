74b4dcf13ad524a97a714bb5c43f1c22691704ec
Daniel Tenorio <dtenorio@openfox.com>
2023-05-30 15:26:59 -0500

    Added basic pages

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+
+const About = () => {
+    return(
+        <>
+            <Nav />
+            <h3>About</h3>
+            <p>Vega Construction is a local construction company founded in Poplar Grove, IL. We serve the surrounding regions with expertise in carpentry.</p>
+        </>
+    )
+}
+
+export default About
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+
+const Home = () => {
+    return(
+        <>
+            <Nav />
+            <h2>Welcome to Vega Construction</h2>
+            <p>Come here for all your carpentry needs, we specialize in framing, deck building/repairing, and siding</p>
+        </>
+    )
+}
+
+export default Home
